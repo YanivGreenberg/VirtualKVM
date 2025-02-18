e23bf011c4be25eaf585bd105f15a95f934c7f5c
Yaniv Greenberg <yanivg1000@gmail.com>
2025-02-18 20:50:18 +0200

    addition of wall tracker

--- change_detection.py
+++ change_detection.py
@@ -16,3 +16,22 @@ class BasicChangeDetection(ChangeDetectionStrategy):
         new_gray = cv2.cvtColor(new_frame, cv2.COLOR_BGR2GRAY)
         diff = cv2.absdiff(old_gray, new_gray)
         return diff.mean() > self.threshold
+
+class ScreenEdgeDetector:
+    def __init__(self, threshold = 10, region = None):
+        self.threshold = threshold
+        self.width = region["width"]
+        self.height = region["height"]
+    
+    def on_move(self, x, y, velocity_x, velocity_y):
+        left, right, down, up = False, False, False, False
+        if x <= self.threshold and velocity_x < 0:
+            left = True
+        elif x >= self.width - self.threshold and velocity_x > 0:
+            right = True
+        if y <= self.threshold and velocity_y < 0:
+            up = True
+        elif y >= self.height - self.threshold and velocity_y > 0:
+            down = True
+        return left,right,up,down
+        
